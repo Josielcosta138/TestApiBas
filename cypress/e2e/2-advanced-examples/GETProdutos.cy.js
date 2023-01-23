e64b1ba850ff08509797ad7112878e15b46427e7
Josielcosta138 <63210548+Josielcosta138@users.noreply.github.com>
2023-01-22 23:20:38 -0300

    Inclusao_1Test

--- cypress/e2e/2-advanced-examples/GETProdutos.cy.js
+++ cypress/e2e/2-advanced-examples/GETProdutos.cy.js
@@ -7,6 +7,14 @@ describe(' --> API PRODUTOS.', () => {
             })
                     .then((response) => {
                     cy.log(JSON.stringify(response.body))
+                    expect(response.status).to.eq(200)
+                    expect(response.body.quantidade).to.eq(2)
+                    expect(response.body.produtos.length).to.eq(2)
+                    expect(response.body.produtos[0]).to.have.all.keys(
+                        'nome','preco','descricao','quantidade','_id'
+                    )
+                    
+
                 });
         });
     });
