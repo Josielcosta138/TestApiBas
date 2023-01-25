eaef2f384c8a399de7234cf4226eb80a295a7cd3
Josielcosta138 <63210548+Josielcosta138@users.noreply.github.com>
2023-01-24 22:27:39 -0300

    Test_ApiUsuario_Produt_Carr_Ok

--- cypress/e2e/2-advanced-examples/GETProdutos.cy.js
+++ cypress/e2e/2-advanced-examples/GETProdutos.cy.js
@@ -26,10 +26,9 @@ describe(' --> API USUARIO.', () => {
 
 
 
-
 describe(' --> API PRODUTOS.', () => {
     context('GET /produtos', () => {
-        it('Verifica o retorno da lista de protudos.', () => {
+        it('Verifica o retorno da lista de produtos.', () => {
             cy.request({
                 method: 'GET',
                 url: 'https://serverest.dev/produtos'
@@ -37,82 +36,16 @@ describe(' --> API PRODUTOS.', () => {
                     .then((response) => {
                     cy.log(JSON.stringify(response.body))
                     expect(response.status).to.eq(200)
-                    expect(response.body.quantidade).to.eq(5)
-                    expect(response.body.produtos.length).to.eq(5)
+                    expect(response.body.quantidade).to.eq(2)
+                    expect(response.body.produtos.length).to.eq(2)
                     expect(response.body.produtos[0]).to.have.all.keys(
                         'nome','preco','descricao','quantidade','_id'
                     )
-                   //expect(response.body.produtos[0].nome).to.equal('Logitech MX Vertical')            
+                    
                 });
         });
     });
 });
-
-
-
-describe('GET - Produtos', () =>{
-        it('Busca de produto através da CONST.', () =>{
-            const resultadoExperado = [
-                {
-                    "nome": "Logitech MX Vertical",
-                    "preco": "470",
-                    "descricao": "Mouse",
-                    "quantidade": "382",
-                    "_id": "BeeJh5lz3k6kSIzA"
-                },
-                {
-                    "nome": "Incrível Aço Bacon",
-                    "preco": "36",
-                    "descricao": "inventore laudantium eos ea est et",
-                    "quantidade": "999",
-                    "_id": "HXeP1O9GbaoMCCbZ"
-                },
-                {
-                    "nome": "Samsung 60 polegadas",
-                    "preco": "5240",
-                    "descricao": "TV",
-                    "quantidade": "49977",
-                    "_id": "K6leHdftCeOJj8BJ"
-                },
-                {
-                    "nome": "Incrível Concreto Carro",
-                    "preco": "766",
-                    "descricao": "saepe dolorem veritatis dolores ducimus doloribus",
-                    "quantidade": "999",
-                    "_id": "n9JNbT0zePNu2oWp"
-                },
-                {
-                    "nome": "Lindo Madeira Sabonete",
-                    "preco": "765",
-                    "descricao": "fugit culpa animi eius voluptates ut",
-                    "quantidade": "999",
-                    "_id": "rt7UqjDco61z5cm8"
-                }
-
-
-            ]      
-
-
-            cy.request({
-                method: 'GET',
-                url: 'http://localhost:3000/produtos'
-              }).then((response) => {
-                expect(response.status).to.equal(200);
-                response.body.produtos.forEach((produto, indice) => {
-                  expect(produto.nome).to.equal(expectedResult[indice].nome)
-                  //expect(produto.descricao).to.equal(expectedResult[indice].descricao)
-                  //expect(produto.preco).to.equal(expectedResult[indice].preco)
-                  //expect(produto.quantidade).to.equal(expectedResult[indice].quantidade)
-                })
-              })
-            
-
-
-        });
-});
-
-
-
 
 
 describe(' --> API CARRINHOS.', () => {
@@ -127,12 +60,17 @@ describe(' --> API CARRINHOS.', () => {
                     expect(response.status).to.eq(200)
                     expect(response.body.quantidade).to.eq(1)
                     expect(response.body.carrinhos.length).to.eq(1)
-                    /*expect(response.body.carrinhos[0]).to.have.all.keys(
-                        'nome','preco','descricao','quantidade','_id'
-                    )*/
+                    expect(response.body.carrinhos[0].produtos[0]).to.have.all.keys(
+                        'idProduto','quantidade','precoUnitario'
+                    )
+                    expect(response.body.carrinhos[0]).to.have.all.keys(
+                        'produtos','precoTotal','quantidadeTotal','idUsuario','_id'
+                    )
+                   
                     
                 });
         });
     });
+
 });
 
